a966c66188ff7796399dbbb74d17952b45c999be
Teo <teo.lairla@iessieradeguara.com>
2020-11-17 17:29:05 +0100

    Lista tareas completa

--- web25-js-listaTareas/app.js
+++ web25-js-listaTareas/app.js
@@ -1,2 +1,21 @@
-let listaTareas  = document.getElementById("listaTareas");
-listaTareas.innerHTML = listaTareas.innerHTML+"<li>Hola</li>";
+function anyadirTarea() {
+    //tomo el valor del input
+    let eTarea = document.getElementById("idTarea");
+    // compruebo si tiene algo de texto
+    if (eTarea.value != "") {
+        //si tiene texto, añado la tarea en la lista
+        anyadirElemento(eTarea.value);
+        //despues de añadir borro el elento input
+        eTarea.value = "";
+    }
+    //si no tiene texto, no hago nada
+}
+//funcion añadir elemento a la lista. 
+//cogera un parámetro y lo añadirá a la lista listaTareas
+function anyadirElemento(textoTarea) {
+    //cojo el elemento de la lista
+    let listaTareas = document.getElementById("listaTareas");
+    //le añado un elemento de lista con el texto
+    // del parámetro textoTarea
+    listaTareas.innerHTML += "<li>" + textoTarea + "</li>";
+}
