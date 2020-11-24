2cb19282e25f97c10883b9a3db570bf0cfabff85
Teo <teo.lairla@iessieradeguara.com>
2020-11-24 19:12:15 +0100

    cambios repaso JS.Hay código comentado

---
+++ repaso-JS-examen/app.js
@@ -0,0 +1,41 @@
+/* let vTexto = "hola";
+let vNumero = 3;
+let vBoolean = true;
+
+alert("Mensaje al usuario");
+
+let retornoConfirm;
+retornoConfirm = confirm("¿Estás seguro?");
+
+if(retornoConfirm == true){
+    alert("Has pulsado 'Aceptar'");
+}else{
+    alert("Has pulsado 'Cancelar'");
+}
+
+*/
+
+let colorSolucion = prompt("Pon el color incognita");
+alert("Estoy pensando en un color...");
+
+
+
+let respuesta = prompt("¿Qué color estoy pensando? Tienes 2 intentos");
+
+if (respuesta == colorSolucion) {
+    alert("has acertado");
+} else {
+    respuesta = prompt("¿Qué color estoy pensando? Tienes 1 intentos");
+    if (respuesta == colorSolucion) {
+        alert("has acertado");
+    }else{
+        alert("oooohhh");
+    }
+
+
+
+}
+
+
+
+
