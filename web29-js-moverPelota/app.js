c7677948ab0ac9603dba129948ec671c8adb3fe4
Teo <teo.lairla@iessieradeguara.com>
2020-12-09 16:16:44 +0100

    Hemos puesto límites al movimiento Hemos añadido aceleración.

--- web29-js-moverPelota/app.js
+++ web29-js-moverPelota/app.js
@@ -1,18 +1,37 @@
 let bola = document.getElementById("bola");
+let vel = 5;
+let ultimaPulsacion = "";
 function moverBola(evento) {
     let posX = bola.x;
     let posY = bola.y;
-    let vel = 5;
+    
+    if (ultimaPulsacion == evento.key) {
+        vel+=1;
+    }else{
+        vel=5;
+    }
+    ultimaPulsacion = evento.key;
+    //Ancho máximo 800
+    //Alto máximo 600
+    console.log(evento.key);
     if (evento.key == "ArrowUp") {
-        bola.style.top = posY - vel + "px";
+        if( posY > 0 ){
+            bola.style.top = posY - vel + "px";
+        }
     }
     else if (evento.key == "ArrowDown") {
-        bola.style.top = posY + vel + "px";
+        if (posY < (window.innerHeight-150)) { //600 menos el ancho de la bola
+            bola.style.top = posY + vel + "px";    
+        }
     }
     else if (evento.key == "ArrowLeft") {
-        bola.style.left = posX - vel + "px";
+        if (posX > 0) {
+            bola.style.left = posX - vel + "px";    
+        }
     }
     else if (evento.key == "ArrowRight") {
-        bola.style.left = posX + vel + "px";
+        if (posX < (window.innerWidth-150)) { //800 menos el ancho de la bola
+            bola.style.left = posX + vel + "px";    
+        }
     }
 }
