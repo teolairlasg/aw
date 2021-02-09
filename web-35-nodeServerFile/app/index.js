1ccfbf6575fe73beba9f5f31e41da35aed232d4f
Teo <teo.lairla@iessieradeguara.com>
2021-02-09 17:55:53 +0100

    web 35

--- web-35-nodeServerFile/app/index.js
+++ web-35-nodeServerFile/app/index.js
@@ -3,21 +3,41 @@ const app = express();
 const puerto = 3000;
 const path = require('path');
 
-let rutaIndex = path.join(__dirname,'html','index.html');
+let rutaMedia = path.join(__dirname, 'media');
+app.use(express.static(rutaMedia));
 
-app.get('/',function (req, res) {
+let rutaIndex = path.join(__dirname, 'html', 'index.html');
+app.get('/', function (req, res) {
     res.sendFile(rutaIndex);
 });
 
-let rutaStyle = path.join(__dirname,'css','style.css');
-app.get('/style.css',function (req, res) {
+let rutaStyle = path.join(__dirname, 'css', 'style.css');
+app.get('/style.css', function (req, res) {
     res.sendFile(rutaStyle);
 });
 
+app.get('/imagenAleatoria', function (req, res) {
+    let imagenes = [ 'img1.png', 'img2.png', 'img3.png' ];
+    let indice = numAleatorio(imagenes.length);
+    res.sendFile(path.join(rutaMedia,'img',imagenes[indice]));
+
+})
 
 let inicioServidor = function () {
     console.log('un cliente se ha conectado');
 }
 
-app.listen(puerto,inicioServidor);
+app.listen(puerto, inicioServidor);
 
+/* // Esto es equivalente a las 6 lineas anteriores
+app.listen(puerto,inicioServidor());
+
+function inicioServidor() {
+    console.log('un cliente se ha conectado');
+}
+*/
+
+function numAleatorio(max) {
+    let decimal=Math.random()*max;
+    return Math.floor(decimal);
+}
