6509b88be938c8c113b310846059ca2754b3ff61
Teo <teo.lairla@iessieradeguara.com>
2021-02-10 16:04:47 +0100

    final plantillas parciales

--- web-37-node-plantillas/index.js
+++ web-37-node-plantillas/index.js
@@ -1,16 +1,27 @@
 const express = require('express');
-
+const path = require('path');
 const app = express();
 const puerto = 3000;
 
+const rutaPublic = path.join(__dirname,'public');
+
 app.set('view engine', 'ejs');
+
+app.use(express.static(rutaPublic));
 
 app.get('/', function (req, res) {
     let datos = {
         nombre: "Juan"
     };
     res.render('inicio', datos);
-})
+});
+
+app.get('/:n', function (req, res) {
+    let datos = {
+        nombre: req.params.n
+    };
+    res.render('inicio', datos);
+});
 
 app.listen(puerto, iniciaServidor());
 function iniciaServidor() {
